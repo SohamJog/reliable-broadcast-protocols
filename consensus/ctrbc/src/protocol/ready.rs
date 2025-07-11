c7cd5e17e9d06afb21966598dd0bec29ed7cd069
Soham Jog <jogsoham2003@gmail.com>
2025-07-11 16:54:23 +0530

    More robust crash behavior for ctrbc

--- consensus/ctrbc/src/protocol/ready.rs
+++ consensus/ctrbc/src/protocol/ready.rs
@@ -7,11 +7,10 @@ use crate::{CTRBCMsg, ProtMsg};
 use crate::Context;
 impl Context {
     // TODO: handle ready
-    pub async fn handle_ready(self: &mut Context, msg: CTRBCMsg, instance_id:usize){
-
+    pub async fn handle_ready(self: &mut Context, msg: CTRBCMsg, instance_id: usize) {
         let rbc_context = self.rbc_context.entry(instance_id).or_default();
 
-        if rbc_context.terminated{
+        if rbc_context.terminated {
             return;
             // RBC Context already terminated, skip processing this message
         }
@@ -28,7 +27,7 @@ impl Context {
         let root = msg.mp.root();
         let ready_senders = rbc_context.readys.entry(root).or_default();
 
-        if ready_senders.contains_key(&msg.origin){
+        if ready_senders.contains_key(&msg.origin) {
             return;
         }
 
@@ -36,14 +35,12 @@ impl Context {
 
         let size = ready_senders.len().clone();
 
-        if size == self.num_faults + 1{
-
+        if size == self.num_faults + 1 {
             // Sent ECHOs and getting a ready message for the same ECHO
-            if rbc_context.echo_root.is_some() && rbc_context.echo_root.clone().unwrap() == root{
-                
-                // No need to interpolate the Merkle tree again. 
+            if rbc_context.echo_root.is_some() && rbc_context.echo_root.clone().unwrap() == root {
+                // No need to interpolate the Merkle tree again.
                 // If the echo_root variable is set, then we already sent ready for this message.
-                // Nothing else to do here. Quit the execution. 
+                // Nothing else to do here. Quit the execution.
 
                 return;
             }
@@ -51,59 +48,66 @@ impl Context {
             let ready_senders = ready_senders.clone();
 
             // Reconstruct the entire Merkle tree
-            let mut shards:Vec<Option<Vec<u8>>> = Vec::new();
-            for rep in 0..self.num_nodes{
-                
-                if ready_senders.contains_key(&rep){
+            let mut shards: Vec<Option<Vec<u8>>> = Vec::new();
+            for rep in 0..self.num_nodes {
+                if ready_senders.contains_key(&rep) {
                     shards.push(Some(ready_senders.get(&rep).unwrap().clone()));
-                }
-
-                else{
+                } else {
                     shards.push(None);
                 }
             }
 
-            let status = reconstruct_data(&mut shards, self.num_faults+1 , 2*self.num_faults);
-            
-            if status.is_err(){
-                log::error!("FATAL: Error in Lagrange interpolation {}",status.err().unwrap());
+            let status = reconstruct_data(&mut shards, self.num_faults + 1, 2 * self.num_faults);
+
+            if status.is_err() {
+                log::error!(
+                    "FATAL: Error in Lagrange interpolation {}",
+                    status.err().unwrap()
+                );
                 return;
             }
 
-            let shards:Vec<Vec<u8>> = shards.into_iter().map(| opt | opt.unwrap()).collect();
-            
+            let shards: Vec<Vec<u8>> = shards.into_iter().map(|opt| opt.unwrap()).collect();
+
             let mut message = Vec::new();
-            for i in 0..self.num_faults+1{
+            for i in 0..self.num_faults + 1 {
                 message.extend(shards.get(i).clone().unwrap());
             }
 
-            let my_share:Vec<u8> = shards[self.myid].clone();
-            
+            let my_share: Vec<u8> = shards[self.myid].clone();
+
             // Reconstruct Merkle Root
             let merkle_tree = construct_merkle_tree(shards, &self.hash_context);
-            if merkle_tree.root() == root{
-                
-                // Ready phase is completed. Save our share for later purposes and quick access. 
-                rbc_context.fragment = Some((my_share.clone(),merkle_tree.gen_proof(self.myid)));
+            if merkle_tree.root() == root {
+                // Ready phase is completed. Save our share for later purposes and quick access.
+                rbc_context.fragment = Some((my_share.clone(), merkle_tree.gen_proof(self.myid)));
 
                 rbc_context.message = Some(message);
 
                 // Insert own ready share
-                rbc_context.readys.get_mut(&root).unwrap().insert(self.myid, my_share.clone());
+                rbc_context
+                    .readys
+                    .get_mut(&root)
+                    .unwrap()
+                    .insert(self.myid, my_share.clone());
                 // Send ready message
-                let ctrbc_msg = CTRBCMsg{
+                let ctrbc_msg = CTRBCMsg {
                     shard: my_share,
                     mp: merkle_tree.gen_proof(self.myid),
                     origin: self.myid,
                 };
-                
+
                 let ready_msg = ProtMsg::Ready(ctrbc_msg.clone(), instance_id);
 
-                self.broadcast(ready_msg).await;
+                if !self.crash {
+                    self.broadcast(ready_msg).await;
+                }
             }
-        }
-        else if size == self.num_nodes - self.num_faults && !rbc_context.terminated {
-            log::info!("Received n-f READY messages for RBC instance id {}, terminating",instance_id);
+        } else if size == self.num_nodes - self.num_faults && !rbc_context.terminated {
+            log::info!(
+                "Received n-f READY messages for RBC instance id {}, terminating",
+                instance_id
+            );
             // Terminate protocol
             rbc_context.terminated = true;
             let term_msg = rbc_context.message.clone().unwrap();
